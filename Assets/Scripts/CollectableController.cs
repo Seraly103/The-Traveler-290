21fa0e29b24b3b389635d6124231df7c5253c387
MrCoconut123 <60995315+MrCoconut123@users.noreply.github.com>
2026-03-11 21:26:16 -0700

    Alice stuff!!

--- Assets/Scripts/CollectableController.cs
+++ Assets/Scripts/CollectableController.cs
@@ -2,15 +2,118 @@ using UnityEngine;
 
 public class CollectableController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private string playerTag = "Player";
+    [SerializeField] private bool useProximityFallback = true;
+    [SerializeField] private float proximityRadius = 1.25f;
+
+    private bool showConfirmation;
+    private bool hasTriggered;
+    private Transform playerTransform;
+
+    private const string PopupMessage = "are you sure? some wonderfully impossible things might happen";
+
+    private void Start()
     {
-        
+        GameObject playerObject = GameObject.FindGameObjectWithTag(playerTag);
+        if (playerObject != null)
+        {
+            playerTransform = playerObject.transform;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!useProximityFallback || hasTriggered || showConfirmation)
+        {
+            return;
+        }
+
+        if (playerTransform == null)
+        {
+            GameObject playerObject = GameObject.FindGameObjectWithTag(playerTag);
+            if (playerObject != null)
+            {
+                playerTransform = playerObject.transform;
+            }
+            return;
+        }
+
+        float distance = Vector3.Distance(playerTransform.position, transform.position);
+        if (distance <= proximityRadius)
+        {
+            TryOpenPopup(playerTransform.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        TryOpenPopup(other.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TryOpenPopup(collision.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TryOpenPopup(other.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        TryOpenPopup(collision.gameObject);
+    }
+
+    private void TryOpenPopup(GameObject otherObject)
+    {
+        if (hasTriggered || showConfirmation)
+        {
+            return;
+        }
+
+        if (!otherObject.CompareTag(playerTag))
+        {
+            return;
+        }
+
+        showConfirmation = true;
+    }
+
+    private void Collect()
+    {
+        hasTriggered = true;
+        showConfirmation = false;
+        Destroy(gameObject);
+    }
+
+    private void OnGUI()
+    {
+        if (!showConfirmation)
+        {
+            return;
+        }
+
+        const float width = 460f;
+        const float height = 180f;
+        Rect windowRect = new Rect((Screen.width - width) * 0.5f, (Screen.height - height) * 0.5f, width, height);
+
+        GUI.Box(windowRect, "Collect Item");
+
+        Rect labelRect = new Rect(windowRect.x + 16f, windowRect.y + 36f, windowRect.width - 32f, 60f);
+        GUI.Label(labelRect, PopupMessage);
+
+        Rect yesRect = new Rect(windowRect.x + 70f, windowRect.y + 110f, 140f, 36f);
+        Rect noRect = new Rect(windowRect.x + windowRect.width - 210f, windowRect.y + 110f, 140f, 36f);
+
+        if (GUI.Button(yesRect, "Yes"))
+        {
+            Collect();
+        }
+
+        if (GUI.Button(noRect, "No"))
+        {
+            showConfirmation = false;
+        }
     }
 }
