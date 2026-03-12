397cc945f5761634019095ddbfffc73999f6f1ff
Seraly103 <seraly103@gmail.com>
2026-03-11 20:18:51 -0700

    test
    test
    
    Co-Authored-By: MrCoconut123 <60995315+mrcoconut123@users.noreply.github.com>

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+    public GameObject world;
+
+    SpriteRenderer sr;
+
+    bool IsWalking = false;
+
+    Animator anim;
+
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        IsWalking = false;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+
+
+
+
+        if(Input.GetKey(KeyCode.A))
         {
-            
+            anim.SetBool("IsWalkingRight", true);
+            anim.SetBool("IsWalkingLeft", false);
+            anim.SetBool("IsWalking", true);
+            world.transform.position += Vector3.right * speed * Time.deltaTime;
+            sr.flipX = false;
         }
+        
+        
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("IsWalkingLeft", false);
+            anim.SetBool("IsWalkingRight", true);
+            anim.SetBool("IsWalking", true);
+            world.transform.position += Vector3.left * speed * Time.deltaTime;
+            sr.flipX = true;
+        }
+        
+        
     }
 }
